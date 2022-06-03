9fd5bdb724f66f845bedc31dfc18befaf8800262
Effrat <effratk@gmail.com>
2022-06-03 16:25:39 +0300

    data filtered by ministers integrated

--- src/format/csv_to_json.py
+++ src/format/csv_to_json.py
@@ -8,5 +8,5 @@ def csv_to_json():
     bill_sponsors = pd.read_csv('../data/exported from PBI/bill_sponsors.csv')
     bill_sponsors.to_json('../data/json/bill_sponsors.json', orient='records')
 
-    faction = pd.read_csv('../data/exported from PBI/faction.csv')
-    faction.to_json('../data/json/faction.json', orient='records')
+    person = pd.read_csv('../data/exported from PBI/person.csv')
+    person.to_json('../data/json/person.json', orient='records')
